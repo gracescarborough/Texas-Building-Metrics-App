ce3cc653489c28158ea3caf3da465f49f1bcbdaa
Grace Scarborough <gracescarborough@utexas.edu>
2026-01-26 20:48:27 -0600

    Fix data paths after moving Streamlit app

--- FloodFiles/map_app.py
+++ FloodFiles/map_app.py
@@ -11,8 +11,8 @@ import os
 st.set_page_config(layout="wide", page_title="Texas Building Metrics")
 
 
-GRID_PATH = "FloodFiles/tx_grid_classified.shp"
-CENTROIDS_PATH = "FloodFiles/sample_centroids_with_density.shp"
+GRID_PATH = "tx_grid_classified.shp"
+CENTROIDS_PATH = "sample_centroids_with_density.shp"
 
 
 @st.cache_data(show_spinner=False)
